08aeef747eb5e065f5d9838b4d681f41ffcf5f45
Valerie Manuel <valerie.manuel11@gmail.com>
2019-08-11 18:39:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,14 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|k,v| self.send(("#{k}="), v)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
+    
     
   end
 
