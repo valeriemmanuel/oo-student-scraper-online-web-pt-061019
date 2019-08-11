e394c1da3ba52b042357e4e455a99e027375551f
Valerie Manuel <valerie.manuel11@gmail.com>
2019-08-11 18:26:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,9 +6,14 @@ class Scraper
   def self.scrape_index_page(index_url)
     student_hash = []
     html =Nokogir::HTML(open(index_url))
-    
+    html.css(".student-card").collect do |student|
+        hash = { name: studnet.css("h4.student-name").text, 
+        location: student.css("p.student-location").text,
+        profile_url: "http://students.learn.co/" + student.csss("a").attritube("href)"}
+        
+        student_hash << hash
   end
-
+  student_has
   def self.scrape_profile_page(profile_url)
     
   end
